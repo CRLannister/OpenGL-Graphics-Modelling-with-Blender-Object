d262bf155dac6ca960b6355bd38c93ac78ded6b7
1001root <sawnhot27.aa@gmail.com>
2019-03-06 17:45:44 +0545

    after ignore

---
+++ obj/cmake-build-debug/CMakeFiles/ICTC.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/CPP_files/Graphics.cc" "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/cmake-build-debug/CMakeFiles/ICTC.dir/CPP_files/Graphics.cc.o"
+  "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/CPP_files/Math.cc" "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/cmake-build-debug/CMakeFiles/ICTC.dir/CPP_files/Math.cc.o"
+  "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/CPP_files/Scene.cc" "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/cmake-build-debug/CMakeFiles/ICTC.dir/CPP_files/Scene.cc.o"
+  "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/CPP_files/Transformation.cc" "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/cmake-build-debug/CMakeFiles/ICTC.dir/CPP_files/Transformation.cc.o"
+  "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/main.cc" "/home/ashish/Documents/Codes/final_graphics/5thSem_Graphics/obj/cmake-build-debug/CMakeFiles/ICTC.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Native_files/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ obj/cmake-build-debug/CMakeFiles/ICTC.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ICTC.
+# This may be replaced when dependencies are built.
